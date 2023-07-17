940ddf11e75091881641a54638c1c995823b7482
saneyoshi <aaa0410as@gmail.com>
2023-07-18 01:42:10 +0900

    fix:MySQL migrate時のエラーを解消。

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+    "go-shopsite/src/database"
+
     "github.com/gofiber/fiber/v2"
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
 )
 
 func main() {
-    _, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/shop_site"), &gorm.Config{})
-
-    if err != nil {
-        panic("Cloud not connect with the database!")
-    }
+    database.Connect()
+    database.AutoMigrate()
 
     app := fiber.New()
 
