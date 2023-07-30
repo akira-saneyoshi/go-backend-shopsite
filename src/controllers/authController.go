4810d3ea5c15fb457132e20f6c8b69952faf81d3
saneyoshi <aaa0410as@gmail.com>
2023-07-31 00:51:52 +0900

    add:middlewares

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"go-shopsite/src/database"
+	"go-shopsite/src/middlewares"
 	"go-shopsite/src/models"
 
 	"github.com/dgrijalva/jwt-go"
@@ -100,24 +101,11 @@ func Login(c *fiber.Ctx) error {
 }
 
 func User(c *fiber.Ctx) error {
-	cookie := c.Cookies("jwt")
-
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("secret"), nil
-	})
-
-	if err != nil || !token.Valid {
-		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"message": "unauthenticated",
-		})
-	}
-
-	payload := token.Claims.(*jwt.StandardClaims)
+	id, _ := middlewares.GetUserId(c)
 
 	var user models.User
 
-	database.DB.Where("id = ?", payload.Subject).First(&user)
+	database.DB.Where("id = ?", id).First(&user)
 
 	return c.JSON(user)
 }
