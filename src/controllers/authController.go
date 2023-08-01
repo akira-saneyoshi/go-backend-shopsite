56501b1b3f465bde9ccf8d1a67cfd3358335bdbe
saneyoshi <aaa0410as@gmail.com>
2023-08-02 00:41:22 +0900

    add:Scopes

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -6,11 +6,10 @@ import (
 	"go-shopsite/src/models"
 	"strings"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 
 	//"golang.org/x/crypto/bcrypt"
-	"strconv"
+
 	"time"
 )
 
@@ -73,12 +72,24 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	payload := jwt.StandardClaims{
-		Subject:   strconv.Itoa(int(user.Id)),
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	isAmbassador := strings.Contains(c.Path(), "/api/ambassador")
+
+	var scope string
+
+	if isAmbassador {
+		scope = "ambassador"
+	} else {
+		scope = "admin"
 	}
 
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte("secret"))
+	if !isAmbassador && user.IsAmbassador {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthorized",
+		})
+	}
+
+	token, err := middlewares.GenerateJWT(user.Id, scope)
 
 	if err != nil {
 		c.Status(fiber.StatusBadRequest)
